c0488b3a40dfb28a88ef3eb66949c2dd564e6519
joh <ly.johann1@gmail.com>
2025-10-12 22:03:24 +0200

    fix dataset

--- src/dataset.py
+++ src/dataset.py
@@ -1,9 +1,9 @@
-from utils import BATCH_SIZE, SIZE, SEED, compute_mean_std
+from src.utils import BATCH_SIZE, SIZE, SEED, compute_mean_std
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms, datasets
 import torch
 
-def get_transform(data_dir, size=SIZE, batch_size=BATCH_SIZE):
+def get_transform(data_dir, batch_size=BATCH_SIZE, size=SIZE):
     mean, std = compute_mean_std(data_dir=data_dir, size=size, batch_size=batch_size)
 
 
@@ -15,21 +15,21 @@ def get_transform(data_dir, size=SIZE, batch_size=BATCH_SIZE):
     return tf
 
 
-def get_dataloaders(train_dir, test_dir, img_size=SIZE, batch_size=BATCH_SIZE, seed=SEED):
-    train_tf = get_transform(train_dir)
-    test_tf = get_transform(test_dir)
+def get_dataloaders(train_dir, test_dir, batch_size, seed=SEED, size=SIZE):
+    train_tf = get_transform(train_dir, batch_size, size)
+    test_tf = get_transform(test_dir, batch_size, size)
 
     train_dataset = datasets.ImageFolder(root=train_dir, transform=train_tf)
     test_dataset = datasets.ImageFolder(root=test_dir, transform=test_tf)
 
-    size_train = 0.8 * len(train_dataset)
+    size_train = int(0.8 * len(train_dataset))
     size_val = len(train_dataset) - size_train
     train_dataset, val_dataset = random_split(dataset=train_dataset,
                                               lengths=[size_train, size_val], 
-                                              generator=torch.Generator.manual_seed(seed))
+                                              generator=torch.Generator().manual_seed(seed))
     # need to learn about worker
-    train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-    val_loader = DataLoader(dataset=val_dataset, batch_size=BATCH_SIZE, shuffle=False)
-    test_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
     return train_loader, val_loader, test_loader
