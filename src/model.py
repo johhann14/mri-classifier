7d30d5f81e89b3992b1338b5ab86c638856b75d3
joh <ly.johann1@gmail.com>
2025-10-12 22:03:47 +0200

    fix model

--- src/model.py
+++ src/model.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch
-from utils import BATCH_SIZE, SIZE, SEED, N_CLASSES
+from src.utils import BATCH_SIZE, SIZE, SEED, N_CLASSES
 
 class SimpleCNN(nn.Module):
     def __init__(self):
